ad1b22a2d944a97b0369eca99da62d76c67ac514
ArjenCole-mbp <jinhesky@hotmail.com>
2018-12-23 22:48:43 +0800

    增加提交表单函数 上传通知信息未遂

--- client/pages/detailApt/detailApt.js
+++ client/pages/detailApt/detailApt.js
@@ -259,5 +259,39 @@ Page({
         console.log(e);
       }
     })
+  },
+
+
+  getFormID: function (e) {
+    console.log(e.detail)
+    var self = this;
+    let _access_token = '5_E1pZJQzTC-lC0r-JJz9wVAZv5Zv22CNtmV_7C1T0sqC9TV7mGE4FTmDX2B0PVM4LaGtaTfXwzfJLnD7fDKTg8DOICJNkKBQgn_Ot2zYmBJyY1g1VXoBNdtwUE0QaP8_9tWlbR-Zq7L1OyrrPKCIjAEAOGM';
+    let url = 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=' + _access_token; 
+    let _jsonData = {
+      access_token: _access_token,
+      touser: app.globalData.userInfo._openid,
+      template_id: 'VRJi_vUV5ddCY_Hbfu29RbukAFlMfVu0mvBh',
+      form_id: e.detail.formId,
+      page: "pages/index/index",
+      data: {
+        "keyword1": { "value": "测试数据一", "color": "#173177" },
+        "keyword2": { "value": "测试数据二", "color": "#173177" },
+        "keyword3": { "value": "测试数据三", "color": "#173177" },
+      }
+    }
+    wx.request({
+      url: url,
+      data: _jsonData,
+      method: 'POST',
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (err) {
+        console.log('request fail ', err);
+      },
+      complete: function (res) {
+        console.log("request completed!");
+      }
+    })
   }
 })
