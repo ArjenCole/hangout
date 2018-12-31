ffc818d0eae75ca880fc97844d22c346ec1f6230
ArjenCole-Desktop <jinhesky@hotmail.com>
2018-12-31 20:21:55 +0800

    新增场馆页面增加场馆类型选择

--- client/pages/siteNew/siteNew.js
+++ client/pages/siteNew/siteNew.js
@@ -5,7 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
+    checkboxItems: [
+      { name: '足球', value: '足球', checked: true },
+      { name: '篮球', value: '篮球' },
+      { name: '网球', value: '网球' },
+      { name: '羽毛球', value: '羽毛球' }
+    ],
 
+    countryCodes: ["+86", "+80", "+84", "+87"],
+    countryCodeIndex: 0,
+
+    countries: ["中国", "美国", "英国"],
+    countryIndex: 0,
+
+    accounts: ["微信号", "QQ", "Email"],
+    accountIndex: 0,
+
+    isAgree: false
   },
 
   /**
@@ -62,5 +78,24 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
+  },
 })
